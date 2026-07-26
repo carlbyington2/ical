7017df5ab4aea508d02bab719c5c4f9a8669f1f7
Carl Byington <carl@five-ten-sg.com>
2026-07-26 10:11:24 -0600

    version 3.0.4

--- dayview.tcl
+++ dayview.tcl
@@ -7,8 +7,10 @@
 # A DayView shows the notices and appointments for one day.
 
 set dayview_id 0
+set ICAL_ICON [file join [file dirname [info script]] contrib ical_icon.png]
 
 class DayView {} {
+    global ICAL_ICON
     # Generate new id for window
     global dayview_id
     incr dayview_id
@@ -47,7 +49,8 @@ class DayView {} {
 
     wm title $n Calendar
     wm iconname $n ical
-    wm iconbitmap $n ical_icon
+    image create photo applicationIcon -file $ICAL_ICON;wm iconphoto $n -default applicationIcon   
+
     wm protocol $n WM_DELETE_WINDOW [list ical_close_view $n]
 
     $self set_date [date today]
