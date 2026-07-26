8f73f80f07c73290d2811e45f88d0f935eb94fa7
Carl Byington <carl@five-ten-sg.com>
2026-07-26 10:11:23 -0600

    add ical-2.3.1-free.patch

--- dateeditor.C
+++ dateeditor.C
@@ -148,12 +148,12 @@ int Cmd_HiliteLoop(ClientData, Tcl_Interp* tcl, int argc, const char* argv[]) {
 	char buffer[20];
 	sprintf(buffer, "%d", list[i].date.EpochDays());
 	if (Tcl_SetVar(tcl, dvar, buffer, 0) == NULL) {
-	    free((char*) strlist);
+	    Tcl_Free((char*) strlist);
 	    TCL_Error(tcl, "could not set loop variable");
 	}
 
 	if (Tcl_SetVar(tcl, hvar, (char*)hilite, 0) == NULL) {
-	    free((char*) strlist);
+	    Tcl_Free((char*) strlist);
 	    TCL_Error(tcl, "could not set loop variable");
 	}
 
@@ -165,11 +165,11 @@ int Cmd_HiliteLoop(ClientData, Tcl_Interp* tcl, int argc, const char* argv[]) {
 	if (result == TCL_BREAK) break;
 
 	// Error of some sort
-	free((char*) strlist);
+	Tcl_Free((char*) strlist);
 	return result;
     }
 
-    free((char*) strlist);
+    Tcl_Free((char*) strlist);
     TCL_Return(tcl, "");
 }
 
