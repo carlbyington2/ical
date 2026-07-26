7e3858b64834c60e74ee2b09123d3f1623e5f0b0
Carl Byington <carl@five-ten-sg.com>
2026-07-26 10:11:23 -0600

    version 2.2

---
+++ patchlevel.h
@@ -0,0 +1,7 @@
+/* Copyright (c) 1996  by Sanjay Ghemawat */
+#ifndef _PATCHLEVEL_H
+#define _PATCHLEVEL_H
+
+#define ICAL_PATCH_LEVEL 1
+
+#endif /* _PATCHLEVEL_H */
---
+++ types/longmap.C
@@ -0,0 +1,3 @@
+/* Copyright (c) 1996  by Sanjay Ghemawat */
+#define HTABLE_IMPLEMENT
+#include "longmap.h"
